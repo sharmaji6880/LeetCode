d60301cc01aef6bf336fd2be14985fe8330827f0
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-07-29 15:16:15 +0530

    Update linked-list-cycle-ii.cpp

--- Medium/linked-list-cycle-ii.cpp
+++ Medium/linked-list-cycle-ii.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        map<ListNode*,int> m;
+        ListNode *current=head;
+        int flag=0;
+        while(current) {
+            m[current]++;
+            current=current->next;
+            if(m.count(current)) {
+                flag=1;
+                break;
+            }
+        }
+        if(flag) {
+            return current;
+        }
+        return nullptr;
+    }
+};

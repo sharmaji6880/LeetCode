50f1f07bf4f776eae9f0a3923210ed4ccab07961
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-08-03 23:41:02 +0530

    Create sort-array-by-parity.cpp

---
+++ Easy/sort-array-by-parity.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        vector<int> even,odd;
+        for(auto x:nums) {
+            if(x%2) {
+                odd.push_back(x);
+            }
+            else {
+                even.push_back(x);
+            }
+        }
+        nums={};
+        for(auto x:even) {
+            nums.push_back(x);
+        }
+        for(auto x:odd) {
+            nums.push_back(x);
+        }
+        return nums;
+    }
+};

356c261b786d596e51361010e4a38d394b36693f
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-10-15 01:15:14 +0530

    Update total-hamming-distance.cpp

--- Medium/total-hamming-distance.cpp
+++ Medium/total-hamming-distance.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    int hamming(int n) {
-        int count=0;
-        while(n) {
-            if(n%2) {
-                count++;
-            }
-            n/=2;
-        }
-        return count;
-    }
     int totalHammingDistance(vector<int>& nums) {
         int sum=0;
         for(int i=0;i<nums.size()-1;i++) {
+            int x=nums[i];
             for(int j=i+1;j<nums.size();j++) {
-                sum+=hamming(nums[i]^nums[j]);
+                sum+=bitset<32>(x^nums[j]).count();
             }
         }
         return sum;

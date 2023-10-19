91e032ad225be3ad2fa73cd754a56b6fef26709e
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-10-19 23:26:05 +0530

    Create backspace-string-compare.cpp

---
+++ Easy/backspace-string-compare.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> s1,s2;
+        for(auto x:s) {
+            if(x=='#') {
+                if(!s1.empty()) {
+                    s1.pop();
+                }
+            }else {
+                s1.push(x);
+            }
+        }
+        for(auto x:t) {
+            if(x=='#') {
+                if(!s2.empty()) {
+                    s2.pop();
+                }
+            }else {
+                s2.push(x);
+            }
+        }
+        if(s1==s2) {
+            return true;
+        }
+        return false;
+
+        
+    }
+};

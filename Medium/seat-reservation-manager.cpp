8379ea84d224268105cbfbc26d584f81c2fc7941
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-11-06 18:00:07 +0530

    Update seat-reservation-manager.cpp

--- Medium/seat-reservation-manager.cpp
+++ Medium/seat-reservation-manager.cpp
@@ -1,46 +1,19 @@
 class SeatManager {
 public:
-    int smallest;
-    vector<int> seats;
+    set<int> unreserved;
     SeatManager(int n) {
-        for(int i=0;i<n;i++) {
-            seats.push_back(0);
+        for(int i=1;i<=n;i++) {
+            unreserved.insert(i);
         }
-        smallest=1;
     }
     
     int reserve() {
-        seats[smallest-1]=1;
-        int ans=smallest;
-        if(smallest==seats.size()) {
-            smallest=-1;
-            return ans;
-        }
-        int flag=0;
-        for(int i=smallest;i<seats.size();i++) {
-            if(seats[i]==0) {
-                smallest=i+1;
-                flag=1;
-                break;
-            }
-        }
-        if(!flag) {
-            smallest=-1;
-        }
-        return ans;
+        int x = *(unreserved.begin());
+        unreserved.erase(x);
+        return x;
     }
     
     void unreserve(int seatNumber) {
-        seats[seatNumber-1]=0;
-        if(smallest==-1) {
-            smallest=seatNumber;
-        }else {
-            if(seatNumber-1 < smallest-1) {
-                smallest=seatNumber;
-            }else if(seatNumber==smallest) {
-                smallest++;
-            }
-        }
-        
+        unreserved.insert(seatNumber);
     }
 };

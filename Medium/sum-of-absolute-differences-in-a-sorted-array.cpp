1fc856a28cdc8566c3fc35a4e38f29cea29c5464
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2023-11-26 16:03:06 +0530

    Update sum-of-absolute-differences-in-a-sorted-array.cpp

--- Medium/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ Medium/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -17,3 +17,28 @@ public:
         return ans;
     }
 };
+
+//This is an optimized approach
+//It solves the problem much faster than the brute force approach mentioned above
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> prefixSum(n);
+        int totalSum=0;
+        for(int i=0;i<n;i++) {
+            if(i==0) {
+                prefixSum[i]=0;
+                totalSum+=nums[i];
+                continue;
+            }
+            prefixSum[i]=prefixSum[i-1]+nums[i-1];
+            totalSum+=nums[i];
+        }
+        vector<int> ans(n);
+        for(int i=0;i<n;i++) {
+            ans[i]=nums[i]*i - prefixSum[i] + (totalSum-prefixSum[i]-nums[i]) - nums[i]*(n-1-i);
+        }
+        return ans;
+    }
+};

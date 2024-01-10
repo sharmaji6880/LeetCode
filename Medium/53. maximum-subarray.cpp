ab760cf3ca314fd18a2fc7fcf3b9f69135c8a21a
Raghav Sharma <103110504+sharmaji6880@users.noreply.github.com>
2024-01-10 16:29:57 +0530

    Update 53. maximum-subarray.cpp

--- Medium/53. maximum-subarray.cpp
+++ Medium/53. maximum-subarray.cpp
@@ -20,3 +20,23 @@ public:
         return maxSum;
     }
 };
+
+// Kadane's Algorithm
+// Time Complexity: O(n)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++) {
+            sum+=nums[i];
+            if(sum > maxSum) {
+                maxSum=sum;
+            }
+            if(sum < 0) {
+                sum=0;
+            }
+        }
+        return maxSum;
+    }
+};

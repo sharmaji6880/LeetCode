f7d6546e0454b9ab66e78d07d49ef28224ebba7d
sharmaji6880 <rs2092002@gmail.com>
2024-02-25 18:03:41 +0530

    Added Brute Force Solution to Problem 1475

--- Easy/1475. final-prices-with-a-special-discount-in-a-shop.cpp
+++ Easy/1475. final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,3 +1,23 @@
+// This is a Brute Force Approach
+// For larger inputs, it might result into TLE
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> finalPrices = prices;
+
+        for(int i=0;i<n;i++) {
+            for(int j=i+1;j<n;j++) {
+                if(prices[j] <= prices[i]) {
+                    finalPrices[i] = prices[i]-prices[j];
+                    break;
+                }
+            }
+        }
+        return finalPrices;
+    }
+};
+
 // This is an optimal Solution using Monotonic Increasing Stack
 class Solution {
 public:

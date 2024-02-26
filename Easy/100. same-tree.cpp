dec0baf54ffb94b5a6143a8d599a033fc22be2e2
sharmaji6880 <rs2092002@gmail.com>
2024-02-26 22:31:15 +0530

    Added Solution to Same Tree

---
+++ Easy/100. same-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==q) {
+            return true;
+        }
+        if(p==NULL || q==NULL) {
+            return false;
+        }
+        if(p->val==q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right)) {
+            return true;
+        }
+        return false;
+    }
+};
